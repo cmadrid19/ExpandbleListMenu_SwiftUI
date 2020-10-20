f8bbaf1c3297c3290879547668c219305099e783
Max <maxim.macari.dam@gmail.com>
2020-10-20 16:38:55 +0200

    Initial Commit

---
+++ ExpandbleListMenu/ExpandbleListMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandbleListMenuApp.swift
+//  ExpandbleListMenu
+//
+//  Created by Maxim Macari on 20/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ExpandbleListMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
